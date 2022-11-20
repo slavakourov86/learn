17a3008b1b1ea4aca6b3b3678d7b86f2e087b17e
slavakourov86 <slava.kourov@gmail.com>
2022-11-20 20:22:45 +0500

    09-10
    09. изменяем HTML с помощью JS
    10. Структура папок

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var firstNameId = 'first-name';
+var firstNameEl = document.getElementById(firstNameId);
+var lastNameId = 'last-name';
+var lastNameEl = document.getElementById(lastNameId);
+var addressId = 'address';
+var addressEl = document.getElementById(addressId);
+var citiesId = 'cities';
+var citiesEl = document.getElementById(citiesId);;
+var hobbiesId = 'hobbies';
+var hobbiesEl = document.getElementById(hobbiesId);
+var avatarWrapperId = 'avatar-wrapper';
+var avatarWrapperEl = document.getElementById(avatarWrapperId);
+var avatarId = 'avatar';
+var avatarEl = document.getElementById(avatarId);
+// window.alert(firstNameEl.value);
+// window.alert(lastNameEl.value);
+firstNameEl.setAttribute('value', 'Andrew')
+window.alert(firstNameEl.className);
+firstNameEl.className = 'first-name default-input error-input';
+avatarEl.src = 'https://avatars.mds.yandex.net/i?id=0da35e50cb4005b229e728e2df58e3592d070bf8-5390940-images-thumbs&n=13';
+hobbiesEl.value = 'У меня нет интересов';
