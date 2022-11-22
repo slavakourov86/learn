b305aed1d2542164767e43a2f995219afa33d41a
slavakourov86 <slava.kourov@gmail.com>
2022-11-22 22:25:19 +0500

    12-13 Функции
    params, return

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+function logValue(id) {
+    var el = document.getElementById(id);
+    console.log(el.value);
+}
+
+function getValue(id) {
+    var el = document.getElementById(id);
+    return el.value;
+}
+
+
 var firstNameId = 'first-name';
 var firstNameEl = document.getElementById(firstNameId);
 var lastNameId = 'last-name';
@@ -5,7 +16,7 @@ var lastNameEl = document.getElementById(lastNameId);
 var addressId = 'address';
 var addressEl = document.getElementById(addressId);
 var citiesId = 'cities';
-var citiesEl = document.getElementById(citiesId);;
+var citiesEl = document.getElementById(citiesId);
 var hobbiesId = 'hobbies';
 var hobbiesEl = document.getElementById(hobbiesId);
 var avatarWrapperId = 'avatar-wrapper';
@@ -13,15 +24,11 @@ var avatarWrapperEl = document.getElementById(avatarWrapperId);
 var avatarId = 'avatar';
 var avatarEl = document.getElementById(avatarId);
 
-function alertValues() {
-    console.log(firstNameEl.value);
-    console.log(lastNameEl.value);
-    console.log(addressEl.value);
+logValue(firstNameId);
+logValue(lastNameId);
+var value = getValue(firstNameId);
 
-}
-
-alertValues();
-alertValues();
+window.alert(value);
 
 
 // console.log(lastNameEl.value);
@@ -29,4 +36,4 @@ firstNameEl.setAttribute('value', 'Andrew')
 // console.logfirstNameEl.className);
 firstNameEl.className = 'first-name default-input error-input';
 avatarEl.src = 'https://avatars.mds.yandex.net/i?id=0da35e50cb4005b229e728e2df58e3592d070bf8-5390940-images-thumbs&n=13';
-hobbiesEl.value = 'У меня нет интересов';
+hobbiesEl.value = 'Мне интересно программирование';
