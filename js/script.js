b71592e7bbcb89458d4e02dfc6e3e76328b0761a
slavakourov86 <slava.kourov@gmail.com>
2022-11-29 21:51:20 +0500

    if, event.currentTarget

--- js/script.js
+++ js/script.js
@@ -2,36 +2,43 @@ var buttonPlus = document.getElementById('buttonPlus');
 var buttonMinus = document.getElementById('buttonMinus');
 var buttonMulti = document.getElementById('buttonMulti');
 var buttonDev = document.getElementById('buttonDev');
+
 var input1 = document.getElementById('number1');
 var input2 = document.getElementById('number2');
 
+function makeOper(parOper) {
+    var Number1 = Number(input1.value);
+    var Number2 = Number(input2.value);
+    if (parOper === '+') {
+        var result = Number1 + Number2;
+        console.log(result);
+    }
+    else if (parOper === '-') {
+        var result = Number1 - Number2;
+        console.log(result);
+    }
+    else if  (parOper === '*') {
+        var result = Number1 * Number2;
+        console.log(result);
+    }
+    else if  (parOper === '/') {
+        var result = Number1 / Number2;
+        console.log(result);
+    }
+    else {
+        var result = 'Operation is unknown';
+        console.log(result);
+        console.log(parOper);
+    }
+}
 
-function onButtonPlusClick() {
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-    var result = number1 + number2;
-    console.log(result);
-}
-function onButtonMinusClick() {
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-    var result = number1 - number2;
-    console.log(result);
-}
-function onButtonMultiClick() {
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-    var result = number1 * number2;
-    console.log(result);
-}
-function onButtonDevClick() {
-    var number1 = Number(input1.value);
-    var number2 = Number(input2.value);
-    var result = number1 / number2;
-    console.log(result);
+function onButtonClick(eventObject) {
+    var clickedEl = eventObject.currentTarget;
+    var oper = clickedEl.innerHTML;
+    makeOper(oper);
 }
 
-buttonPlus.addEventListener('click', onButtonPlusClick);
-buttonMinus.addEventListener('click', onButtonMinusClick);
-buttonMulti.addEventListener('click', onButtonMultiClick);
-buttonDev.addEventListener('click', onButtonDevClick);
+buttonPlus.addEventListener('click', onButtonClick);
+buttonMinus.addEventListener('click', onButtonClick);
+buttonMulti.addEventListener('click', onButtonClick);
+buttonDev.addEventListener('click', onButtonClick);
